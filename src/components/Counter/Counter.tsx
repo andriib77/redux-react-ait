1754b62c011f4d13cd6211e34c90d8481c144528
Andrii <143963266+andriib77@users.noreply.github.com>
2024-03-22 17:24:28 +0100

    lesson 31

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,4 +1,4 @@
-import Button from "../Button/Button"
+import Button from "components/Button/Button"
 import { useState } from "react"
 
 import { CounterWrapper, ButtonControl, Count } from "./styles"
